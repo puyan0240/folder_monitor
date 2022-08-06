99ff35bd4a6341c856a1c30543c19a939f82c32c
puyan0240 <matusima0604@zeus.eonet.ne.jp>
2022-08-06 20:05:43 +0900

    setup.datに仮想環境とパッケージインストールを追加

--- setup.bat
+++ setup.bat
@@ -3,4 +3,14 @@ if exist ".\venv" (
     echo already setup !!
 ) else (
     echo setup starting ...
+    python -m venv venv
+
+    call .\venv\Scripts\activate.bat
+    python -m pip install -U pip
+    python -m pip install -r requirement.txt
+    call deactivate.bat
+
+    echo ------------------------------------------
+    echo setup complete !!
+    echo ------------------------------------------
 )
